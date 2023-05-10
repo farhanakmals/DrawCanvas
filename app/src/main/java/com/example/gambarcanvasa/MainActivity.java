3b2f381712f987fa6aa034685489c853713c5f95
farhanakmals <shaleh.farhan@gmail.com>
2023-05-10 14:56:21 +0700

    initial commit

---
+++ app/src/main/java/com/example/gambarcanvasa/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gambarcanvasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MyCanvasView myCanvasView = new MyCanvasView(this);
+        setContentView(myCanvasView);
+    }
+}
